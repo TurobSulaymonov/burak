3a918d38f83730b1bcd1ca1facb4b198b5b6d493
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-03-20 13:46:44 +0900

    fix: process Signup

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -6,6 +6,30 @@ console.log("Signup frontend javascript file");
 }); */
 
 $(function(){
+    fileTarget = $(".file-box .upload-hidden");
+    let filename;
+    
+    fileTarget.on("change", function(){
+      if(window.FileReader) {
+        const uploadFile  = $(this) [0].files[0];
+        console.log("uploadFile", uploadFile);
+        const fileType = uploadFile["type"];
+        const validimageType =  ["image/jpg", "image/jpeg", "image/png"];
+        if(!validimageType.includes(fileType)) {
+            alert("Please only jpg, jpeg and png!");
+        }
+        else {
+            if(uploadFile) {
+                console.log(URL.createObjectURL(uploadFile));
+                $(".upload-img-frame")
+                .attr("src", URL.createObjectURL(uploadFile))
+                .addClass("successs");
+            }
+            filename = $(this)[0].files[0].name;
+        }
+        $(this).siblings("upload-name").val(filename);
+      }
+    });
 
 });
 
@@ -28,5 +52,13 @@ function validateSignupForm(){
       alert("Password differs, please check");
       return false;
     }
+
+    const memberImage = $(".member-image").get(0).files[0].name
+          ? $(".member-image").get(0).files[0].name
+          : null;
+          if(!memberImage){
+            alert("Please insert restaurant image!");
+            return false;
+          }
   
 }
