477e81643b8da91640e56c742a6b8c2faa43acfd
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-04-10 20:22:21 +0900

    feat: create view schema model

---
+++ src/models/View.service.ts
@@ -0,0 +1,12 @@
+import ViewModel from "../schema/View.model";
+
+class ViewServes{
+    private readonly viewModel;
+
+    constructor() {
+        this.viewModel = ViewModel;
+    }
+
+}
+
+export default ViewServes;
