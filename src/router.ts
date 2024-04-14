b396cf9ef15e25f3f7e78af85994bd35178d3f5a
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-04-14 17:54:05 +0900

    feat: develop createOrder rest api

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader"
 import productController from "./controllers/product.controller";
-
+import orderController from "./controllers/order.controller";
 /** Member **/
 router.get("/member/restaurant", memberController.getRestaurant)
 router.post("/member/login", memberController.login);
@@ -34,6 +34,10 @@ productController.getProduct
 );
 
 /** Order **/
+router.post("/order/create", 
+memberController.verifyAuth,
+orderController.createOrder
+)
 
 
 
