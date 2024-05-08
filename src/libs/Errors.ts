7e22fb002aac529df6ce0cc37a05558be3921103
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-05-08 20:16:55 +0900

    fix: ZJ task yechimi

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -18,8 +18,6 @@ export enum Message {
   CREATE_FAILED = "Create is failed!",
   UPDATE_FAILED = "Update is failed!",
   
-
-
   USED_NICK_PHONE = "You are inserting already used nick  or phone!",
   NO_MEMBER_NICK = "No member with that member nick!",
   BLOCKED_USER = "You have been blocked, contact the restaurant!",
